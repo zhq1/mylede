b201945095d04fda8184fcd72652ad1cea74e4d0
zhq <32982289+zhq1@users.noreply.github.com>
2021-08-30 19:44:38 +0800

    Update lede.sh

--- lede.sh
+++ lede.sh
@@ -1,1 +1,13 @@
+#添加三方插件dnsfilter
+git clone https://github.com/kiddin9/luci-app-dnsfilter.git /feeds/luci/applications/luci-app-dnsfilter
 
+#添加三方插件OpenClash（本插件是一个可运行在 OpenWrt 上的 Clash 客户端兼容 Shadowsocks、ShadowsocksR、Vmess、Trojan、Snell 等协议，根据灵活的规则配置实现策略代理
+git clone https://github.com/vernesong/OpenClash.git /feeds/luci/applications/luci-app-openclash
+
+#passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git /feeds/luci/applications/luci-app-passwall
+
+./scripts/feeds update luci
+./scripts/feeds install -a -p luci
+./scripts/feeds update packages
+./scripts/feeds install -a -p packages
